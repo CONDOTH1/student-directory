ef654abb00e8c635591d108a052f7d21625bc172
Thady Condon <thadycondon@hotmail.com>
2016-06-21 18:50:09 +0100

    Broke the required actions up into methods. Three methods were added print_header, print(names) and print_footer.

--- directory.rb
+++ directory.rb
@@ -12,7 +12,19 @@ students = [
   "Norman Bates"
 ]
 
+def print_header
  puts "The students of Villains Academy"
  puts "-----------"
- students.each{|student| puts student}
- puts  "Overall, we have #{students.count} great students"
+end
+
+ def print(names)
+ names.each{|student| puts student}
+ end
+
+ def print_footer(names)
+ puts  "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
