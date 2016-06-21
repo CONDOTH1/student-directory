f199adc7535d9685e074290ac33900c00e1446c5
Thady Condon <thadycondon@hotmail.com>
2016-06-21 20:44:56 +0100

    Completed eexercises 1 & 2included. Adding an index to each student in def print(names) and showing names that start with a specific letter, also in def print(names).

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates",cohort: :november}
-]
+
+def student_input
+puts "Please enter the name of students"
+puts "To finish, just press enter twice"
+name = gets.chomp
+students = []
+while !name.empty? do
+  students << {name: name, cohort: :november}
+  puts "Now we have #{students.count} students"
+  name = gets.chomp
+end
+students
+end
 
 def print_header
  puts "The students of Villains Academy"
@@ -18,13 +18,14 @@ def print_header
 end
 
  def print(names)
- names.each{|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
+ names.each.with_index(1){|student, index| puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].split('').shift == "H"}
  end
 
  def print_footer(names)
  puts  "Overall, we have #{names.count} great students"
 end
 
+students = student_input
 print_header
 print(students)
 print_footer(students)
