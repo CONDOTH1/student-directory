c91fb648545a51b0b72f80afacfabf9b3392045d
Thady Condon <thadycondon@hotmail.com>
2016-06-21 21:00:13 +0100

    Completed exercise 4. This involved replacing the .each method in def print(name) with an until loop.

--- directory.rb
+++ directory.rb
@@ -18,9 +18,17 @@ def print_header
 end
 
  def print(names)
+
 #exercise 1, 2, & 3 completed in the below code block:
-names.each.with_index(1){|student, index| puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].split('').shift == "H" && student[:name].length < 12}
- end
+#names.each.with_index(1){|student, index| puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].split('').shift == "H" && student[:name].length < 12}
+
+#exercise 4 below:
+integer = 0
+until integer == names.count
+puts "#{names[integer][:name]} (#{names[integer][:cohort]} cohort)"
+integer += 1
+end
+end
 
  def print_footer(names)
  puts  "Overall, we have #{names.count} great students"
