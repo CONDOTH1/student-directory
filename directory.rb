6dfbd4646bb506aff7df3f45aedb40fb0eba4e19
Thady Condon <thadycondon@hotmail.com>
2016-06-24 18:43:44 +0100

    Completed more exercises 3. I know there is more that could be done but im at my limit of knowledge now. I've split the interactive menu method up into individual methods. I kept the hobbies, place of birth etc and put them through the on method using variables. I've also attempt to reduce lines without changing the flow with limited success.

--- directory.rb
+++ directory.rb
@@ -1,40 +1,56 @@
 @students = []
+@months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
 
 def student_input
-puts "Please enter the name of students"
-puts "To finish, just press enter twice"
-name = STDIN.gets.gsub("\n", '')
-#students = []
-cohort_by_months = []
-months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-while !name.empty? do
-puts "Please enter the student's cohort"
-cohort = STDIN.gets.gsub("\n", '')
-cohort = Time.now.strftime("%B") if cohort.empty?
-until months.include?(cohort.capitalize)
-  puts "Spelling error, please type cohort again!"
-  cohort = STDIN.gets.gsub("\n", '')
-end
-puts "Please enter the students hobbies"
-hobbies = STDIN.gets.gsub("\n", '')
-hobbies = "undisclosed" if hobbies.empty?
-puts "Please enter the students place of birth"
-pob = STDIN.gets.gsub("\n", '')
-pob = "undisclosed" if pob.empty?
-puts "Please enter the students height"
-height = STDIN.gets.gsub("\n", '')
-height = "undisclosed" if height.empty?
+  name = student_name
+  cohort = student_cohort
+  hobbies = student_details("hobbies")
+  pob = student_details("pob")
+  height = student_details("height")
   students_capture(name, cohort, hobbies, pob, height)
-  #@students << {name: name, cohort: cohort, hobbies: hobbies, pob: pob, height: height}
   puts @students.count > 1 ? "Now we have #{@students.count} students" : "Now we have #{@students.count} student"
+  cohort_by_months = sort_by_cohort
+  @students = cohort_by_months
+end
+
+def student_name
+  puts "Please enter the name of students"
+  puts "To finish, just press enter twice"
   name = STDIN.gets.gsub("\n", '')
+  while name.empty? do
+    puts "Please enter a name"
+    name = STDIN.gets.gsub("\n", '')
+  end
+  name
 end
-x = 0
-while x < months.count
-@students.map{|hash| cohort_by_months << hash if hash[:cohort] == months[x]}
-x += 1
+
+def student_cohort
+  puts "Please enter the student's cohort"
+  cohort = STDIN.gets.gsub("\n", '')
+  cohort = Time.now.strftime("%B") if cohort.empty?
+  until @months.include?(cohort.capitalize)
+    puts "Spelling error, please type cohort again!"
+    cohort = STDIN.gets.gsub("\n", '')
+  end
+  cohort
 end
-@students = cohort_by_months
+
+def student_details(string)
+  puts "Please enter the students #{string}"
+  string = STDIN.gets.gsub("\n", '')
+  string = "undisclosed" if string.empty?
+  string
+end
+
+def sort_by_cohort
+  x = 0
+  cohort_by_months = []
+  while x < @months.count
+    @students.map{|hash| cohort_by_months << hash if hash[:cohort] == @months[x]}
+    x += 1
+  end
+  cohort_by_months
 end
 
 def print_header
@@ -42,22 +58,16 @@ def print_header
  puts "-----------".center(500)
 end
 
- def print_students_list
-
-#exercise 1, 2, & 3 completed in the below code block:
-#names.each.with_index(1){|student, index| puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].split('').shift == "H" && student[:name].length < 12}
-
-#exercise 4 below:
-int = 0
-until int == @students.count
-puts "#{@students[int][:name]} (#{@students[int][:cohort]} cohort), Hobbies: #{@students[int][:hobbies]}, Born: #{@students[int][:pob]}, Height: #{@students[int][:height]}".center(500)
-int += 1
-end
+def print_students_list
+  int = 0
+    until int == @students.count
+      puts "#{@students[int][:name]} (#{@students[int][:cohort]} cohort), Hobbies: #{@students[int][:hobbies]}, Born: #{@students[int][:pob]}, Height: #{@students[int][:height]}".center(500)
+      int += 1
+    end
 end
 
  def print_footer
    puts @students.count > 1 ? "Overall, we have #{@students.count} great students".center(500) : "Overall, we have #{@students.count} great student".center(500)
- #puts  "Overall, we have #{names.count} great students".center(500)
 end
 
 def print_menu
@@ -118,7 +128,6 @@ end
     file.readlines.each do |line|
       name, cohort, hobbies, pob, height = line.chomp.split(',')
       students_capture(name, cohort, hobbies, pob, height)
-      #@students << {name: name, cohort: cohort, hobbies: hobbies, pob:pob, height:height}
     end
     file.close
   end
